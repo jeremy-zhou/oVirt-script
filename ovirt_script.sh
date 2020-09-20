8d6c865a2c766333015d07c74b6231ff88a96a83
jeremy_zhou <feng4206yu@163.com>
2020-09-20 18:54:48 +0800

    Update ovirt_script.sh

--- ovirt_script.sh
+++ ovirt_script.sh
@@ -3,8 +3,6 @@
 yum -y install openssh-devel elfutils-libelf-devel bc
 yum -y groupinstall "development tools"
 yum -y install ncurses-devel openssl-devel
-scp root@10.231.208.89:/root/linux-4.19.145.tar.xz /root/
-scp root@10.231.208.89:/root/NVIDIA-Linux-x86_64-418.66-vgpu-kvm.run /root/
 tar xvf linux-4.19.145.tar.xz
 cd linux-4.19.145
 make menuconfig
