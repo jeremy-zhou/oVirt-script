bacf033d1a0f0451bcb9bec26ed51e67b1f089f3
jeremy_zhou <feng4206yu@163.com>
2020-09-20 14:26:27 +0800

    Update nouveau.sh

--- nouveau.sh
+++ nouveau.sh
@@ -4,5 +4,5 @@ echo "blacklist nouveau " >> /etc/modprobe.d/blacklist-nouveau.conf
 echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
 mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r).img.bak
 dracut -v /boot/initramfs-$(uname -r).img $(uname -r)
-cp cpu-tune.py /usr/libexec/vdsm/hooks/before_vm_start/
+cp ./oVirt-script/cpu-tune.py /usr/libexec/vdsm/hooks/before_vm_start/
 reboot
