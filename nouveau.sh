501f6539225c3590048af232fe665f185676a461
jeremy_zhou <feng4206yu@163.com>
2020-09-20 14:23:13 +0800

    Update nouveau.sh

--- nouveau.sh
+++ nouveau.sh
@@ -4,5 +4,5 @@ echo "blacklist nouveau " >> /etc/modprobe.d/blacklist-nouveau.conf
 echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
 mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r).img.bak
 dracut -v /boot/initramfs-$(uname -r).img $(uname -r)
-cpu cpu-tune.py /usr/libexec/vdsm/hooks/before_vm_start/
+cp cpu-tune.py /usr/libexec/vdsm/hooks/before_vm_start/
 reboot
